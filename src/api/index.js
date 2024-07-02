fc86b4c3ca930e645069cab5350bfd5771874c10
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-07-02 12:20:36 +0100

    edited

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 
-const emojis = require('./emojis');
+const hello = require("./hello");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: "Welcome to my server!",
   });
 });
 
-router.use('/emojis', emojis);
+router.use("/hello", hello);
 
 module.exports = router;
