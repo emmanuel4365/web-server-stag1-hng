394cff22207cb52f1b99d9ef3751347e20ef71f7
emmanuel4365 <128937350+emmanuel4365@users.noreply.github.com>
2024-07-02 13:11:55 +0100

    edited app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const axios = require("axios");
 // const helmet = require("helmet");
 // const cors = require("cors");
 
@@ -15,10 +16,36 @@ app.use(morgan("dev"));
 // app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to my server!",
-  });
+app.get("/api/hello", async (req, res) => {
+  try {
+    let queryData = req.query;
+    // console.log(queryData);
+
+    let clientIP = req.socket.remoteAddress || req.headers["x-forwarded-for"];
+
+    let ipResponse = await axios.get("https://ipapi.co/json/");
+
+    let location = ipResponse;
+    // console.log(location);
+
+    let weather = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location.data.city}&appid=fec2e968bd33b3fd04023b0be3ecd320&units=metric`
+    );
+
+    // console.log(weather);
+
+    res.status(200).json({
+      client_ip: clientIP,
+      location: location.data.city,
+      greeting: `Hello, ${
+        queryData.visitor_name
+      }!, the temperature is ${Math.ceil(
+        weather.data.main.temp
+      )} degrees Celcius in ${location.data.city}`,
+    });
+  } catch (error) {
+    res.status(500).send("Something went wrong! Please check and try again.");
+  }
 });
 
 app.use("/api", api);
